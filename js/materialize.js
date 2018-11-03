cf74289d8723ca3a83fac49d81111556247a5ce1
Sydney <sydneyjberry12@gmail.com>
2018-11-03 01:13:18 -0500

    Implemented side navigation for mobile

--- js/materialize.js
+++ js/materialize.js
@@ -9,6 +9,9 @@
    console.log('callback - particles.js config loaded');
  });
 
+ $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
